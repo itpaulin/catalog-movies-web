d1589f4a4059e9fe99ac9d00d67a07c2a3a27d1a
Paulo Ricardo <prmaltaleal@gmail.com>
2024-08-11 15:50:46 -0300

    feat: add theme provider and queryclient provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme.ts'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { MovieContextProvider } from './context/MovieContextProvider.tsx'
 
+const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <MovieContextProvider>
+          <App />
+        </MovieContextProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
