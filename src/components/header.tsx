1e52fab88f73b4121daaaa42044b2547998cc9f8
Paulo Ricardo <prmaltaleal@gmail.com>
2024-08-12 19:53:39 -0300

    refactor: put header inside stack

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,16 +2,14 @@ import { Stack, Button } from '@mui/material'
 import { useMovieContext } from '../context/useMovieContext'
 import { forwardRef } from 'react'
 
-export const Header = forwardRef<HTMLDivElement>((props, ref) => {
+export const Header = forwardRef<HTMLDivElement>((_, ref) => {
   const { reindexMovies } = useMovieContext()
 
   return (
-    <header>
-      <Stack ref={ref}>
-        <Button variant="contained" size="large" onClick={reindexMovies}>
-          Reload Movies
-        </Button>
-      </Stack>
-    </header>
+    <Stack ref={ref} component={'header'}>
+      <Button variant="contained" size="large" onClick={reindexMovies}>
+        Reload Movies
+      </Button>
+    </Stack>
   )
 })
