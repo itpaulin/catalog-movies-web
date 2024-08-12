0f249fbb04f355b9f13a6d0397af47c169fa891a
Paulo Ricardo <prmaltaleal@gmail.com>
2024-08-12 15:09:16 -0300

    feat: ensure user can go to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Container, Fab } from '@mui/material'
+import { Fragment, useRef } from 'react'
+import { MovieList } from './components/movie-list'
+import { Header } from './components/header'
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const headerRef = useRef<HTMLDivElement>(null)
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Fragment>
+      <Container sx={{ overflowX: 'hidden' }}>
+        <Header ref={headerRef} />
+        <MovieList />
+        <Fab
+          color="primary"
+          aria-label="Go to top"
+          sx={{
+            position: 'fixed',
+            display: 'block',
+            bottom: 16,
+            right: 16,
+            zIndex: 1,
+          }}
+          onClick={() =>
+            headerRef.current?.scrollIntoView({ behavior: 'smooth' })
+          }
+        >
+          <ArrowUpwardIcon />
+        </Fab>
+      </Container>
+    </Fragment>
   )
 }
 
